2bb634fdda1251d52974c19650363d43944a026a
sofra123 <55323904+sofra123@users.noreply.github.com>
2020-06-23 21:11:48 +0200

    last

--- index.js
+++ index.js
@@ -331,9 +331,6 @@ app.get("*", function(req, res) {
   res.sendFile(__dirname + "/index.html");
 });
 
-// server.listen(8080, function() {
-//   console.log("I'm listening.");
-// });
-
-const port = process.env.PORT || 8080;
-app.listen(port);
+server.listen(process.env.PORT || 8080, function() {
+  console.log("I'm listening.");
+});
